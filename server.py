9e265535b2808506e8aec644821f395d095ea84a
Rylie Johnson <ryliexjohnson@gmail.com>
2021-11-25 12:42:22 -0500

    updated server to handle multiple clients. need to test more

--- server.py
+++ server.py
@@ -9,29 +9,26 @@ end a run with this code, you will need to use CTRL+C for both
 the server and the client.
 '''
 
+def threader(server_socket) -> None:
+    server_socket.listen()
+    client_sock, client_addr = server_socket.accept()
+    print(f"[{ctime()}] Connected to client {client_addr}.")
+
+    sender_thread = Thread(target=sender, args=(client_sock, "server_dir"))
+    reciever_thread = Thread(target=receiver, args=(client_sock, "server_dir"))
+
+    sender_thread.start()
+    reciever_thread.start()
+
 
 def main(argv) -> None:
     # Initialize the server socket that a client will connect to.
     server_socket = socket(AF_INET, SOCK_STREAM)
     server_socket.bind((argv.host, argv.port))
 
-    # Wait to establish a connection with a client that tries to connect.
-    server_socket.listen()
-    client_sock, client_addr = server_socket.accept()
-    print(f"[{ctime()}] Connected to client {client_addr}.")
+    threader(server_socket)
+    threader(server_socket)
 
-    # Initialize the threads for both sending/receiving functionalities and then
-    # start the threads. The purpose of this is to allow the server to have a more
-    # seamless communication with the client on the other end. Otherwise, communication
-    # becomes more complicated.
-    sender_thread = Thread(target=sender, args=(client_sock, "server_dir"))
-    receiver_thread = Thread(target=receiver, args=(client_sock, "server_dir"))
-
-    # Start and join the threads.
-    sender_thread.start()
-    receiver_thread.start()
-    sender_thread.join()
-    receiver_thread.join()
 
 
 if __name__ == "__main__":
